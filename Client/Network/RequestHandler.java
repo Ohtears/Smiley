d1cd01dc9c3c4b4f78572f8177d82af12e9abcfc
Tears <ashkanmarali@gmail.com>
2024-06-09 23:54:56 +0330

    commit for socket 1

--- Client/Network/RequestHandler.java
+++ Client/Network/RequestHandler.java
@@ -1,5 +1,21 @@
 package Client.Network;
 
+import java.io.*;
+import java.net.*;
+
 public class RequestHandler {
-    
+
+    private int portnumber = 12345;
+
+    private String ipaddress = "localhost";
+
+    public void main(String[] args) {
+
+    try (Socket socket = new Socket(ipaddress, portnumber)) {
+            System.out.println("Connected to server.");
+    } 
+    catch (IOException e) {
+            System.out.println("Client exception: " + e.getMessage());
+        }
+    }
 }
--- Server/Network/ResponseHandler.java
+++ Server/Network/ResponseHandler.java
@@ -1,5 +1,26 @@
 package Server.Network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class ResponseHandler {
-    
+ 
+    private int portnumber = 12345;
+
+
+    public void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(portnumber)) {
+            System.out.println("Server started...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress());
+        
+
+        
+        } catch (IOException e) {
+            System.out.println("Server exception: " + e.getMessage());
+        }
+    }
+
 }
