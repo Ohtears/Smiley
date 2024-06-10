cef4732cb69616e1776c58faf9626cae0837589d
Tears <ashkanmarali@gmail.com>
2024-06-10 21:12:39 +0330

    added request types

--- Server/Services/Message.java
+++ Server/Services/Message.java
@@ -1,5 +1,50 @@
 package Server.Services;
 
+import java.sql.Timestamp;
+
 public class Message {
-    
+    private int messageId;
+    private int senderId;
+    private int receiverId;
+    private String content;
+    private Timestamp timestamp;
+
+    public Message(int messageId, int senderId, int receiverId, String content, Timestamp timestamp) {
+        this.messageId = messageId;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "messageId=" + messageId +
+                ", senderId=" + senderId +
+                ", receiverId=" + receiverId +
+                ", content='" + content + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
--- Server/Services/User.java
+++ Server/Services/User.java
@@ -1,5 +1,47 @@
 package Server.Services;
 
-public class User {
-    
+
+import Server.Database.MYSQLHandler;
+
+public class User{
+
+    private int userid;
+    public String Username;
+    private String Email;
+    private String Password;
+    private TimeDate Birthday;
+    private String Name;
+    private String bio;
+
+    public User(int userid, String Username, String Name, String Email, String Password, TimeDate Birthday, String bio){
+
+        this.userid = userid;
+        this.Username = Username;
+        this.Name = Name;
+        this.Email = Email;
+        this.Password = Password;
+        this.Birthday = Birthday;
+        this.bio = bio;
+        
+    }
+
+    public int getID(){
+
+        return userid;
+    }
+
+    public String getName() {
+
+        return Name;
+    }
+
+    public TimeDate getBirthday() {
+
+        return Birthday;
+    }
+
+    public String getBio() {
+
+        return bio;
+    }
 }
