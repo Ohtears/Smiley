8f874943794ce14c8e5a6b20a1971ce5ca28fed4
Tears <ashkanmarali@gmail.com>
2024-06-10 21:12:39 +0330

    added request types

---
+++ Server/Services/TimeDate.java
@@ -0,0 +1,76 @@
+package Server.Services;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+public class TimeDate {
+    
+    private int day;
+    private int month;
+    private int year;
+
+    public TimeDate(int day, String monthString, int year) {
+        this.day = day;
+        this.month = month2int(monthString); 
+        this.year = year;
+    }
+
+    public TimeDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        this.day = calendar.get(Calendar.DAY_OF_MONTH);
+        this.month = calendar.get(Calendar.MONTH) + 1; 
+        this.year = calendar.get(Calendar.YEAR);
+    }
+
+    public TimeDate(TimeDate date) {
+        this.day = date.day;
+        this.month = date.month; 
+        this.year = date.year;
+    }
+
+    private int month2int(String month){
+
+        String[] months = {"January", "February", "March", "April", "May", "June", "July",
+                           "August", "September", "October", "November", "December"};
+        for (int i = 0; i < months.length; i++) {
+            if (months[i].equalsIgnoreCase(month)) {
+                return i + 1;
+            }
+        }
+        return 0;
+
+    }
+    @Override
+    public String toString(){
+
+        return day + ":" + month + ":" + year;
+
+    }
+
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
