c553e2f1dfd9d0169ab9b8fd4e0d976f85be70f8
Tears <ashkanmarali@gmail.com>
2024-06-11 17:02:36 +0330

    adding json convertors to both client and server

--- Server/Network/JsonConverter.java
+++ Server/Network/JsonConverter.java
@@ -3,6 +3,8 @@ package Server.Network;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import Client.Models.User;
+import Client.Network.RequestType;
 import Server.Services.TimeDateService;
 import Server.Services.UserService;
 
@@ -36,4 +38,28 @@ public class JsonConverter {
 
         return new UserRequest(users, requestType);
     }
+
+    public static JSONObject usersToJson(List<User> users, RequestType type) {
+        JSONObject requestJson = new JSONObject();
+        requestJson.put("requestType", type.toString());
+        
+        JSONArray jsonArray = new JSONArray();
+        for (User user : users) {
+            JSONObject userJson = new JSONObject();
+            userJson.put("userid", user.getID());
+            userJson.put("Username", user.Username);
+            userJson.put("Name", user.getName());
+            userJson.put("Email", user.getEmail());
+            userJson.put("Password", user.getPassword());
+            userJson.put("Birthday", user.getBirthday().toString());
+            userJson.put("bio", user.getBio());
+            
+            jsonArray.put(userJson);
+        }
+        
+        requestJson.put("users", jsonArray);
+        return requestJson;
+    }
+
+
 }
