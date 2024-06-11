06ce417ca41e04d8362de62869b7cf458e6ffbdc
Tears <ashkanmarali@gmail.com>
2024-06-11 17:02:36 +0330

    adding json convertors to both client and server

--- Client/Network/JsonConverter.java
+++ Client/Network/JsonConverter.java
@@ -4,7 +4,12 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import Client.Models.User;
+import Server.Network.RequestTypeService;
+import Server.Network.UserRequest;
+import Server.Services.TimeDateService;
+import Server.Services.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonConverter {
@@ -29,5 +34,30 @@ public class JsonConverter {
         
         requestJson.put("users", jsonArray);
         return requestJson;
+    }
+        public static UserRequest jsonToUsers(JSONObject jsonObject) {
+        RequestTypeService requestType = RequestTypeService.valueOf(jsonObject.getString("requestType"));
+        JSONArray jsonArray = jsonObject.getJSONArray("users");
+
+        List<UserService> users = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject userJson = jsonArray.getJSONObject(i);
+
+            int userid = userJson.getInt("userid");
+            String Username = userJson.getString("Username");
+            String Name = userJson.getString("Name");
+            String Email = userJson.getString("Email");
+            String Password = userJson.getString("Password");
+            String Birthday = userJson.getString("Birthday");
+            TimeDateService bday = new TimeDateService(Birthday);
+
+            String bio = userJson.getString("bio");
+
+            UserService user = new UserService(userid, Username, Name, Email, Password, bday, bio);
+            users.add(user);
+        }
+
+        return new UserRequest(users, requestType);
     }
 }
