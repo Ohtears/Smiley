cdb49301fde2418a40e6edc8476023f5de7c8b74
Tears <85892741+Ohtears@users.noreply.github.com>
2024-06-20 00:49:13 +0330

    added smil;ey icon/added home button

--- Client/GUI/MainApp/ForumsListPanel.java
+++ Client/GUI/MainApp/ForumsListPanel.java
@@ -3,14 +3,34 @@ package Client.GUI.MainApp;
 import java.awt.*;
 import javax.swing.*;
 
+import Client.GUI.MainApp.Dashboard.PanelSwitchListener;
+
 public class ForumsListPanel extends JPanel {
 
-    public ForumsListPanel() {
+    public ForumsListPanel(PanelSwitchListener listner) {
         setBorder(BorderFactory.createTitledBorder("Forums List"));
         setBackground(new Color(32, 34, 37));
-        setLayout(new GridLayout(2, 1));
+        setLayout(new BorderLayout());
 
-        JLabel label1 = new JLabel("Coming soon");
-        add(label1);
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER)); 
+        topPanel.setBackground(new Color(32, 34, 37));
+
+        HomeButton homeButton = new HomeButton(listner);
+        topPanel.add(homeButton);
+
+        JPanel bottomPanel = new JPanel(new GridLayout(0, 1));
+        bottomPanel.setBackground(new Color(32, 34, 37));
+
+        for (int i = 0; i < 5; i++) {
+            JLabel forumButton = new JLabel("Forum " + (i + 1)); 
+            forumButton.setHorizontalAlignment(SwingConstants.CENTER);
+            forumButton.setForeground(Color.WHITE); 
+            forumButton.setPreferredSize(new Dimension(75, 75));
+            bottomPanel.add(forumButton);
+        }
+
+        add(topPanel, BorderLayout.NORTH);
+        add(bottomPanel, BorderLayout.CENTER);
     }
+
 }
